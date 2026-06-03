39399af52a1170b42771355fded694b817bae701
Bryan <buungoc1805@gmail.com>
2026-06-03 01:08:42 +0000

    feat: add AdminUserController

---
+++ src/Controllers/AdminUserController.php
@@ -0,0 +1,23 @@
+<?php
+class AdminUserController {
+    private function checkAdmin() {
+        if (session_status() === PHP_SESSION_NONE) session_start();
+
+        if (!isset($_SESSION["user_id"]) || !isset($_SESSION["user_role"])) {
+            header("Location: /login");
+            exit;
+        }
+
+        if ($_SESSION["user_role"] !== "admin" && $_SESSION["user_role"] !== "staff") {
+            die("403 - No access.");
+        }
+    }
+
+    public function index() {
+        $this->checkAdmin();
+        $customers = User::getAllCustomers();
+        require_once __DIR__ . "/../Views/admin/users/index.php";
+    }
+
+    
+}
